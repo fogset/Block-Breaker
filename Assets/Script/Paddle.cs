96af8bf036cbc066d1ab98eb29bfdae89c720a8c
fogset <zhangth1990@hotmail.com>
2018-12-16 23:37:38 -0800

    restrict the mouse position

--- Assets/Script/Paddle.cs
+++ Assets/Script/Paddle.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour {
-
+    //configuration partamaters
     [SerializeField] float screenWidthInUnits = 16f;
-  
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+
     // Use this for initialization
     void Start () {
 		
@@ -14,7 +16,8 @@ public class Paddle : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
         transform.position = paddlePos;
 	}
 }
