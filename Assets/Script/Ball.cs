35e54c03b749b7cc73181832deeb88fb2f572675
fogset <zhangth1990@hotmail.com>
2018-12-17 22:21:10 -0800

    added ball script

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,7 +17,23 @@ public class Ball : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        LockBallToPaddle();
+        LaunchOnMouseClick();
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 15f);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
-	}
+    }
+
+    
 }
