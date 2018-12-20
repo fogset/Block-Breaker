de023e8e5d8093b4b6b577492126f843e6ee2536
fogset <zhangth1990@hotmail.com>
2018-12-19 23:41:44 -0800

    load next scene when blocks are destroyed

--- Assets/Script/Level.cs
+++ Assets/Script/Level.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Level : MonoBehaviour {
-
+    //parameters
     [SerializeField] int breakableBlocks;  //Serialized for debugging purposes
-       
+
+    //cached reference
+    SceneLoader sceneLoader;
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
         Debug.Log("block" + breakableBlocks);
+    }
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if(breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
     }
 }
