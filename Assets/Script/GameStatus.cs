b4f60c2a3fc615a6546c3d24560938e670f69291
fogset <zhangth1990@hotmail.com>
2018-12-20 23:59:02 -0800

    display score on the screen

--- Assets/Script/GameStatus.cs
+++ Assets/Script/GameStatus.cs
@@ -1,22 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour {
     //config params
     [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestoryed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     //state variables
     [SerializeField] int currentScore = 0;
 
-	// Update is called once per frame
-	void Update () {
+    private void Start()
+    {
+        scoreText.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update () {
         Time.timeScale = gameSpeed;
 	}
 
     public void AddToScore()
     {
         currentScore = currentScore + pointsPerBlockDestoryed;
+        scoreText.text = currentScore.ToString();
     }
 }
