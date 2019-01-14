2408518d83c41ad40d3e988a4c44def2c216ad19
fogset <zhangth1990@hotmail.com>
2019-01-13 23:51:15 -0800

    manage block damage levels

--- Assets/Script/Block.cs
+++ Assets/Script/Block.cs
@@ -3,29 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour {
-
+    // config params
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] int maxHits;
     //cached reference
     Level level;
     GameStatus gameStatus;
 
+    //state variables
+    [SerializeField] int timesHit; //TODO only serialized ofr debug purposes
     private void Start()
     {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
         level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameStatus>();
         if (tag == "Breakable")
         {
             level.CountBlocks();
         }
-        gameStatus = FindObjectOfType<GameStatus>();
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(tag == "Breakable")
         {
+            HandleHit();
+        }
+
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
             DestoryBlock();
         }
-        
     }
 
     private void DestoryBlock()
